09e137776b3610dcf995e57ddcf44ccdf9339232
Keenan McDonald <keemcd@gmail.com>
2020-11-08 08:48:57 -0600

    mobile fixes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://terra-server1.herokuapp.com/',
-    //API_ENDPOINT: 'http://localhost:8000/',
+    //API_ENDPOINT: 'https://terra-server1.herokuapp.com/',
+    API_ENDPOINT: 'http://localhost:8000/',
     TOKEN_KEY: 'token-key',
 }
